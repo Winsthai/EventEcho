dd39d7d3d4d8ad2751a53834a5ac36f486a1540b
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-11-17 01:25:02 -0700

    login page start

--- client/src/Components/LoginPage/LoginPage.jsx
+++ client/src/Components/LoginPage/LoginPage.jsx
@@ -1,5 +1,36 @@
+import Grid2 from '@mui/material/Grid2';
+import Box from '@mui/material/Box';
+
+import './LoginPageStyles.css';
+
 const LoginPage = () => {
-  return <div>login</div>;
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <Grid2 container spacing ={2} column={8}>
+        <Grid2 size={6}>
+          <h1>EventEcho</h1>
+        </Grid2>
+      </Grid2>
+    </Box>
+    
+    
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <Grid container spacing={2}>
+    //     <Grid size={8}>
+    //       <Item>size=8</Item>
+    //     </Grid>
+    //     <Grid size={4}>
+    //       <Item>size=4</Item>
+    //     </Grid>
+    //     <Grid size={4}>
+    //       <Item>size=4</Item>
+    //     </Grid>
+    //     <Grid size={8}>
+    //       <Item>size=8</Item>
+    //     </Grid>
+    //   </Grid>
+    // </Box>
+  );
 };
 
 export default LoginPage;
