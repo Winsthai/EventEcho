648d8546c9ce2b201996455ab6aaa5ac28f05358
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-11-17 04:14:40 -0700

    Steven: added basic ui for login page

--- client/src/Components/LoginPage/LoginPage.jsx
+++ client/src/Components/LoginPage/LoginPage.jsx
@@ -1,36 +1,69 @@
-import Grid2 from '@mui/material/Grid2';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import PasswordBox from '../PasswordBox';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 import './LoginPageStyles.css';
 
+import logo from '../../images/logo.png'
+
 const LoginPage = () => {
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <Grid2 container spacing ={2} column={8}>
-        <Grid2 size={6}>
-          <h1>EventEcho</h1>
-        </Grid2>
-      </Grid2>
+    <Box 
+      component="section"
+      sx={{
+        width:"100%",
+        height: "100%",
+        display: "flex",
+        justifyContent:"center",
+        alignItems: "center", 
+        textAlign:"center",
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          width: "85%",
+          height: "100%"
+        }}
+      >
+        <Stack
+          direction="column"
+          spacing={5.5}
+          sx={{
+            height:"100%",
+            marginTop: "10vh",
+          }}
+        >
+          <h1 id='loginHeader'>EventEcho</h1>
+
+          <div>
+            <img src={logo} alt="EventEcho Logo" style={{width:'150px', marginBottom:'-4vh'}}/>
+          </div>
+
+          <TextField id="loginUser" label="Username" variant="standard" />
+          <PasswordBox></PasswordBox>
+
+          <Stack 
+            direction="row"
+            sx={{
+              justifyContent: "space-between",
+            }}
+          >
+            <Button variant="text">Create Account</Button>
+            <Button variant="contained">Next</Button>
+          </Stack>
+          
+          <Button variant="text">Stay on Guest Mode</Button>
+        </Stack>
+      
+      </Box>
     </Box>
     
     
-    // <Box sx={{ flexGrow: 1 }}>
-    //   <Grid container spacing={2}>
-    //     <Grid size={8}>
-    //       <Item>size=8</Item>
-    //     </Grid>
-    //     <Grid size={4}>
-    //       <Item>size=4</Item>
-    //     </Grid>
-    //     <Grid size={4}>
-    //       <Item>size=4</Item>
-    //     </Grid>
-    //     <Grid size={8}>
-    //       <Item>size=8</Item>
-    //     </Grid>
-    //   </Grid>
-    // </Box>
   );
 };
+
 
 export default LoginPage;
---
+++ client/src/Components/PasswordBox.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export default function PasswordBox() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box>
+      <div>
+        <FormControl sx={{ m: 0, width: '100%' }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label={
+                    showPassword ? 'hide the password' : 'display the password'
+                  }
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  onMouseUp={handleMouseUpPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
+    </Box>
+  );
+}
