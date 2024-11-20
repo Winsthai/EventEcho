1d8994070600220396dcd37f4416f9575db3a4ce
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-11-20 04:34:38 -0700

    feat: filters bar added

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,13 @@
 import SearchBar from "../SearchBar";
 import { Box, Stack, Button, useMediaQuery} from "@mui/material";
+
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import GroupsIcon from '@mui/icons-material/Groups';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 
 import './HomePageStyles.css';
 const HomePage = () => {
@@ -18,13 +25,8 @@ const HomePage = () => {
         >
           <h1>Events</h1>
           <Box>
-            <Button variant="contained" sx={{borderRadius:"20px",}}>
-              <Box
-                id="homeLoginButton"
-              >
-               <AccountCircleIcon></AccountCircleIcon>Login
-              </Box>
-              
+            <Button variant="contained" sx={{borderRadius:"20px",}} startIcon={<AccountCircleIcon/>}>
+              <Box id="homeLoginButton"> Login </Box>   
             </Button>
           </Box>
           
@@ -36,12 +38,64 @@ const HomePage = () => {
           <SearchBar></SearchBar>
           <Stack
             direction="row"
+            id="homeFiltersStack"
           >
             <Box id="homeFiltersBox">
-              <Button variant="contained" sx={{borderRadius:"20px",}}>Login</Button>
-            </Box>
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", backgroundColor:"#ff7474"}} 
+                startIcon={<SportsBasketballIcon/>}
+              >
+                Sports 
+              </Button>
+
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", 
+                backgroundColor:"#ff7474"}} 
+                startIcon={<MusicNoteIcon/>}
+              > 
+                Music 
+              </Button>
+
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", backgroundColor:"#ff7474"}} 
+                startIcon={<LocalDiningIcon/>}
+              > 
+                Food 
+              </Button>
+              
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", backgroundColor:"#ff7474"}} 
+                startIcon={<ColorLensIcon/>}
+              > 
+                Art
+              </Button>
+              
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", backgroundColor:"#ff7474"}}
+                startIcon={<GroupsIcon/>}
+              > 
+                Hangout
+              </Button>
+
+              <Button 
+                variant="contained" 
+                sx={{borderRadius:"20px", backgroundColor:"#ff7474"}} 
+                startIcon={<SportsEsportsIcon/>}
+              > 
+                Gaming
+              </Button>
+            </Box>           
           </Stack>
 
+          <Box>
+            <h1> Upcoming Events</h1>
+          </Box>
+        
           
         </Stack>
         
