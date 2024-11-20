4af14989377133a5ec172ecc81c5565b57d381d1
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-11-20 03:36:06 -0700

    feat: search bar component added, home page started, imports optimized

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,5 +1,59 @@
+import SearchBar from "../SearchBar";
+import { Box, Stack, Button, useMediaQuery} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import './HomePageStyles.css';
 const HomePage = () => {
-  return <div>home</div>;
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  if (isMobile) {
+    // Mobile Component
+    return (
+      <Box
+        id="homeBox"
+      >
+        <Stack
+          direction="row"
+          id="homeHeaderStack"
+        >
+          <h1>Events</h1>
+          <Box>
+            <Button variant="contained" sx={{borderRadius:"20px",}}>
+              <Box
+                id="homeLoginButton"
+              >
+               <AccountCircleIcon></AccountCircleIcon>Login
+              </Box>
+              
+            </Button>
+          </Box>
+          
+        </Stack>
+  
+        <Stack
+          id="homeEventsStack"
+        >
+          <SearchBar></SearchBar>
+          <Stack
+            direction="row"
+          >
+            <Box id="homeFiltersBox">
+              <Button variant="contained" sx={{borderRadius:"20px",}}>Login</Button>
+            </Box>
+          </Stack>
+
+          
+        </Stack>
+        
+      </Box>
+      
+    );
+  } else {
+    return (
+      <div>in progress</div>
+    );
+  }
+  
 };
 
 export default HomePage;
