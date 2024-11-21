dea6295541ce56087a1cc65ede035bbba6b6917e
AhmedElshabasi <ahmed.elshabasi@ucalgary.ca>
2024-11-21 16:25:21 -0700

    feat: desktop version of create new guest complete

--- client/src/Components/CreateEventPage/AddNewGuestPage.jsx
+++ client/src/Components/CreateEventPage/AddNewGuestPage.jsx
@@ -1,43 +1,20 @@
 import * as React from 'react';
 import {
   AppBar, Toolbar, IconButton, Typography,
-  Box, Stack, Button, useMediaQuery, Checkbox, TextField,
+  Box, Stack, Button, useMediaQuery, TextField,
   Avatar
 } from '@mui/material';
 import { BottomNavigationAction } from "@mui/material";
 import "./DesktopInviteGuests.css";
 import "./MobileInviteGuests.css";
-//import arrowIcon from '../../images/arrow button.png';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import {useState} from "react";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
-
-const contacts = [
-  { id: 1, name: "Steven Nguyen", phone: "(403)-000-0000" },
-  { id: 2, name: "Winston Thai", phone: "(403)-111-1111" },
-  { id: 3, name: "Shaun Tapiau", phone: "(403)-222-2222" },
-  { id: 4, name: "Ahmed Elshabasi", phone: "(403)-333-3333" },
-  { id: 5, name: "Desmond Lau", phone: "(403)-444-4444" },
-];
 
 
 export default function DesktopAddGuestsPage() {
   const onEditPage = location.pathname.includes("edit");
   console.log(onEditPage);
-
-  const [selectedContacts, setSelectedContacts] = useState([]);
   const isMobile = useMediaQuery("(max-width:600px)");
-
-  const handleSelect = (id) => {
-    setSelectedContacts((prev) =>
-      prev.includes(id)
-        ? prev.filter((contactId) => contactId !== id)
-        : [...prev, id]
-    );
-  };
 
 
   // Mobile Layout
@@ -97,8 +74,8 @@ const MobileLayout = () => (
               height: "100%",
               padding: 2,
               display: "flex",
-              justifyContent: "center", // Center vertically inside the stack
-              alignItems: "center",    // Center horizontally inside the stack
+              justifyContent: "center", 
+              alignItems: "center",    
                }}
           >
             <Avatar
@@ -119,9 +96,9 @@ const MobileLayout = () => (
         {/* Camera Icon Overlay */}
         <IconButton
           sx={{
-            position: 'fixed', // Use fixed positioning for the screen
-            top: '22%',        // Center vertically
-            left: '57%',       // Center horizontally
+            position: 'fixed', 
+            top: '22%',        
+            left: '57%',       
             backgroundColor: 'white',
             border: '1px solid #ccc',
           }}
@@ -169,69 +146,118 @@ const MobileLayout = () => (
 
   // Desktop Layout
   const DesktopLayout = () => (
-    <div className="desktop-container">
-      <div className="desktop-content">
-        <div className="desktop-search-and-add">
-            <input
-                type="text"
-                placeholder="   Search Contacts"
-                className="search-input"
-            />
-            <BottomNavigationAction
-                icon={<PersonAddIcon />}
-                className="add-icon"
-            />
-        </div>
+    <Box component="form" sx={{
+      width:"100%",
+      height: "100%",
+      pt: 12,
+      pb: 8,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center", 
+      alignItems: "center",    
+    }}>
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{ width: "40%", height: "100%", padding: 2 }}
+          >
 
-        <h2 className = "your-friends-text">Your Friends</h2>
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{ width: "100%",
+              height: "100%",
+              padding: 2,
+              display: "flex",
+              justifyContent: "center", 
+              alignItems: "center",    
+               }}
+          >
+            <Typography variant="h5" component="div" sx={{ flexGrow: 0, pb: 3 }}>
+                Add New Guest
+              </Typography>
+            <Avatar
+          sx={{
+            width: 200,
+            height: 200,
+            backgroundColor: '#FFB4B4',
+            fontSize: 42,
+            color: '#EC3E39',
+            fontWeight: 'bold',    
+          }}
+        >
+          Guest
+        </Avatar>
+      </Stack>
 
         
-        <div className="desktop-contactGrid">
-          {contacts.map((contact) => (
-            <div key={contact.id} className="desktop-contactCard">
-                <Checkbox
-                  checked={selectedContacts.includes(contact.id)}
-                  onChange={() => handleSelect(contact.id)}
-                  inputProps={{ 'aria-label': `Select ${contact.name}` }}
-                  icon={<RadioButtonUncheckedIcon />} // Circular unchecked icon
-                  checkedIcon={<CheckCircleIcon />}   // Circular checked icon
-                />
-              <div className="desktop-avatar">{contact.name[0]}</div>
-              <div>
-                <p className="desktop-name">{contact.name}</p>
-                <p className="desktop-phone">{contact.phone}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        
-        <Box
-  sx={{
-    display: "flex", // enables flexbox for centering
-    justifyContent: "center", // horizontally centers the button
-    alignItems: "center", // vertically centers the button
-    pl: 10, // optional padding on the left, remove or adjust as needed
-    pt: 20,
-  }}
->
-  <Button
-    variant="contained"
-    sx={{
-      borderRadius: "10px",
-      width: "25%", // button width
-      padding: "1rem", // button height
-      backgroundColor: "#F68F8D",
-      "&:hover": {
-        backgroundColor: "#A50B07",
-      },
-    }}
-  >
-    Review Event Details
-  </Button>
-</Box>
-        
-      </div>
-    </div>
+        {/* Camera Icon Overlay */}
+        <IconButton
+          sx={{
+            position: 'fixed', 
+            top: '32%',        
+            left: '57%',       
+            backgroundColor: 'white',
+            border: '1px solid #ccc',
+          }}
+          size="large"
+        >
+          <PhotoCameraIcon fontSize="small" />
+        </IconButton>
+            <Box sx={{ display: "flex", justifyContent: "left" }}>
+              Name *
+            </Box>
+
+            <TextField fullWidth required id="event-title" label="Enter name" variant="outlined" sx={{ width: "100%" }} />
+
+            <Box sx={{ display: "flex", justifyContent: "left" }}>
+              E-mail *
+            </Box>
+
+            <TextField fullWidth required id="event-title" label="Enter e-mail address" variant="outlined" sx={{ width: "100%" }} />
+
+            <Box sx={{ display: "flex", justifyContent: "left" }}>
+              Phone
+            </Box>
+
+            <TextField fullWidth required id="event-title" label="Enter phone" variant="outlined" sx={{ width: "100%" }} />
+            
+          <Stack
+              direction="row"
+              spacing={8}
+              sx={{ width: "100%",
+                height: "100%",
+                padding: 2,
+                display: "flex",
+                justifyContent: "center", 
+                alignItems: "center",    
+                }}
+            >
+            <Button variant='contained'
+              sx={{
+                borderRadius: '10px',
+                backgroundColor: "#F68F8D",
+                "&:hover": {
+                  backgroundColor: "#A50B07",
+                }
+              }}
+            >
+              Cancel
+            </Button>
+            <Button variant='contained'
+          sx={{
+            borderRadius: '10px',
+            backgroundColor: "#F68F8D",
+            "&:hover": {
+              backgroundColor: "#A50B07",
+            }
+          }}
+        >
+          Save Guest
+            </Button>
+          </Stack>
+      </Stack>
+    </Box>
   );
 
   return isMobile ? <MobileLayout /> : <DesktopLayout />;
