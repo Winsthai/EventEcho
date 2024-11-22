2cf32204b51c5620517b9b8a9238920e4b56bd95
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2024-11-22 14:20:37 -0700

    feat: desktop filters complete

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -7,9 +7,8 @@ import {
   Button,
   useMediaQuery,
   FormControl,
-  Select,
+  Menu,
   MenuItem,
-  InputLabel,
 } from "@mui/material";
 import { useState } from "react";
 
@@ -21,48 +20,48 @@ import ColorLensIcon from "@mui/icons-material/ColorLens";
 import GroupsIcon from "@mui/icons-material/Groups";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
-import { useParams, useNavigate } from 'react-router-dom';
 
+import { useParams, useNavigate } from "react-router-dom";
 
 import "./HomePageStyles.css";
 
 const events = [
   {
-    "id": "1",
-    "title": "Football Game",
-    "eventtype": "Sports",
-    "description": "A friendly neighborhood football game.",
-    "address": "123 Stadium Rd, City",
-    "coordinates": {
-      "x": 40.7128,
-      "y": -74.006
+    id: "1",
+    title: "Football Game",
+    eventtype: "Sports",
+    description: "A friendly neighborhood football game.",
+    address: "123 Stadium Rd, City",
+    coordinates: {
+      x: 40.7128,
+      y: -74.006,
     },
-    "startdate": "2024-11-15T00:00:00.000Z",
-    "starttime": "15:00:00+00",
-    "enddate": "2024-11-15T00:00:00.000Z",
-    "endtime": "17:00:00+00",
-    "visibility": true,
+    startdate: "2024-11-15T00:00:00.000Z",
+    starttime: "15:00:00+00",
+    enddate: "2024-11-15T00:00:00.000Z",
+    endtime: "17:00:00+00",
+    visibility: true,
     image:
       "https://m.media-amazon.com/images/M/MV5BOWZiNzZkZGEtMWEwOS00NjZkLWFmYTctZmQyMDY3NGU0OWZjXkEyXkFqcGc@._V1_.jpg", // temporary
   },
   {
-    "id": "2",
-    "title": "Jazz Concert",
-    "eventtype": "Music",
-    "description": "Live jazz performance.",
-    "address": "456 Music Hall Ave, City",
-    "coordinates": {
-      "x": 40.7306,
-      "y": -73.9352
+    id: "2",
+    title: "Jazz Concert",
+    eventtype: "Music",
+    description: "Live jazz performance.",
+    address: "456 Music Hall Ave, City",
+    coordinates: {
+      x: 40.7306,
+      y: -73.9352,
     },
-    "startdate": "2024-12-01T00:00:00.000Z",
-    "starttime": "19:00:00+00",
-    "enddate": "2024-12-01T00:00:00.000Z",
-    "endtime": "21:00:00+00",
-    "visibility": true,
+    startdate: "2024-12-01T00:00:00.000Z",
+    starttime: "19:00:00+00",
+    enddate: "2024-12-01T00:00:00.000Z",
+    endtime: "21:00:00+00",
+    visibility: true,
     image:
       "https://www.horizonsmusic.co.uk/cdn/shop/articles/image1_1600x1600.jpg?v=1621417277", // temporary
-  }
+  },
 ];
 
 const HomePage = () => {
@@ -115,15 +114,16 @@ const HomePage = () => {
 
   const commonEvents = getCommonEvents();
 
-  const [open, setOpen] = useState(false);
-
-  const handleDropdownClick = () => {
-    setOpen ((prev) => !prev);
-  }
+  // Filter menu const
+  const [anchorE1, setAnchorE1] = useState(null);
+  const open = Boolean(anchorE1);
+  const handleDropdownClick = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
 
   const handleDropdownClose = () => {
-    setOpen(false);
-  }
+    setAnchorE1(null);
+  };
 
   if (isMobile) {
     // Mobile Component
@@ -136,8 +136,9 @@ const HomePage = () => {
 
           {/* Login button */}
           <Box>
-            <Button variant="contained"
-              onClick={() => navigate('/user/1')}
+            <Button
+              variant="contained"
+              onClick={() => navigate("/user/1")}
               sx={{ borderRadius: "20px" }}
               startIcon={<AccountCircleIcon />}
             >
@@ -264,39 +265,130 @@ const HomePage = () => {
     return (
       <Stack direction="column" id="homeDesktopStack">
         <Stack direction="row" sx={{ alignItems: "center" }}>
-          <SearchBar></SearchBar>
-          <FormControl sx={{ minWidth: 50 }}>
-            <Button
-              variant="contained"
-              sx={{
-                borderRadius: "20px",
-                backgroundColor: "#A50B07"
-              }}
-              startIcon={<FilterAltIcon/>}
-            >
-              Filters
-            </Button>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={[]}
-              label="Age"
-              // onChange={handleChange}
-              autoWidth
-              multiple
-            >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-          </FormControl>
+          <SearchBar onSearchChange={handleSearchChange}></SearchBar>
+
+          <Button
+            id="desktopFiltersButton"
+            variant="contained"
+            sx={{
+              borderRadius: "20px",
+              backgroundColor: "#A50B07",
+            }}
+            startIcon={<FilterAltIcon />}
+            aria-controls={open ? "filtersMenu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleDropdownClick}
+          >
+            Filters
+          </Button>
+          <Menu
+            id="filtersMenu"
+            anchorEl={anchorE1}
+            open={open}
+            onClose={handleDropdownClose}
+            MenuListProps={{
+              "aria-labelledby": "desktopFiltersButton",
+            }}
+          >
+            {/* Sports */}
+            <MenuItem onClick={() => handleFilterClick("Sports")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Sports")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<SportsBasketballIcon />}
+              >
+                Sports
+              </Button>
+            </MenuItem>
+            {/* Music */}
+            <MenuItem onClick={() => handleFilterClick("Music")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Music")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<MusicNoteIcon />}
+              >
+                Music
+              </Button>
+            </MenuItem>
+            {/* Food */}
+            <MenuItem onClick={() => handleFilterClick("Food")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Food")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<LocalDiningIcon />}
+              >
+                Food
+              </Button>
+            </MenuItem>
+            {/* Art */}
+            <MenuItem onClick={() => handleFilterClick("Art")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Art")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<ColorLensIcon />}
+              >
+                Art
+              </Button>
+            </MenuItem>
+            {/* Hangout */}
+            <MenuItem onClick={() => handleFilterClick("Hangout")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Hangout")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<GroupsIcon />}
+              >
+                Hangout
+              </Button>
+            </MenuItem>
+            {/* Gaming */}
+            <MenuItem onClick={() => handleFilterClick("Gaming")}>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: "20px",
+                  backgroundColor: activeFilters.includes("Gaming")
+                    ? "#A50B07"
+                    : "#ff7474",
+                }}
+                startIcon={<SportsEsportsIcon />}
+              >
+                Gaming
+              </Button>
+            </MenuItem>
+          </Menu>
         </Stack>
 
         <h1 style={{ marginTop: "0" }}>Upcoming Events</h1>
 
-        {events.length !== 0 ? (
+        {commonEvents.length !== 0 ? (
           <>
-            {events.map((event) => (
+            {commonEvents.map((event) => (
               <EventCard key={event.id} event={event} variant="" />
             ))}
           </>
