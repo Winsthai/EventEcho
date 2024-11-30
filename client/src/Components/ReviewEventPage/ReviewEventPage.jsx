e4ad0c2b7f31020819de283fcba11bf00e279782
shauntapiau <155854109+shauntapiau@users.noreply.github.com>
2024-11-30 16:32:28 -0700

    fix: changed review event mobile layout to look like event details mobile.

--- client/src/Components/ReviewEventPage/ReviewEventPage.jsx
+++ client/src/Components/ReviewEventPage/ReviewEventPage.jsx
@@ -14,8 +14,15 @@ import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import dayjs from 'dayjs';
-import logo from '../../images/logo.png';
+import dayjs from "dayjs";
+import logo from "../../images/logo.png";
+
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import LocalDiningIcon from "@mui/icons-material/LocalDining";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 
 import "./ReviewEventPageStyles.css";
 
@@ -84,7 +91,12 @@ const contacts = [
   { id: 5, name: "Desmond Lau", phone: "(403)-444-4444" },
 ];
 
-const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invitedGuests }) => {
+const ReviewEventPage = ({
+  eventDetails,
+  setEventDetails,
+  detailsCompleted,
+  invitedGuests,
+}) => {
   const { id } = useParams();
   const navigate = useNavigate();
   const isMobile = useMediaQuery("(max-width:600px)");
@@ -107,25 +119,26 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
       endTimeTrimmed = events[id - 1].endtime.slice(0, -6);
       reviewTime = startTimeTrimmed.concat(" - ", endTimeTrimmed);
       databaseImage = events[id - 1].image;
-    }
-    else {
+    } else {
       startTimeTrimmed = eventDetails.starttime.slice(0, -6);
       // if there is end time
       if (eventDetails.endtime !== null) {
         endTimeTrimmed = eventDetails.endtime.slice(0, -6);
         reviewTime = startTimeTrimmed.concat(" - ", endTimeTrimmed);
         console.log("end time exists");
-      }
-      else {
+      } else {
         reviewTime = startTimeTrimmed.concat(" - TBD");
       }
 
       // if there is end date
-      if (eventDetails.enddate !== null &&
-        !dayjs(JSON.parse(eventDetails.startdateraw)).isSame(dayjs(JSON.parse(eventDetails.enddateraw)))) {
+      if (
+        eventDetails.enddate !== null &&
+        !dayjs(JSON.parse(eventDetails.startdateraw)).isSame(
+          dayjs(JSON.parse(eventDetails.enddateraw))
+        )
+      ) {
         reviewDate = eventDetails.startdate.concat(" - ", eventDetails.enddate);
-      }
-      else {
+      } else {
         reviewDate = eventDetails.startdate;
       }
 
@@ -137,10 +150,13 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
   const handlePostEvent = async (url) => {
     // upload to cloud
     if (eventDetails.imageform !== null) {
-      const res = await fetch(`https://api.cloudinary.com/v1_1/dk7v80lgt/image/upload`, {
-        method: "POST",
-        body: eventDetails.imageform
-      });
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/dk7v80lgt/image/upload`,
+        {
+          method: "POST",
+          body: eventDetails.imageform,
+        }
+      );
 
       const uploadedImageURL = await res.json();
       console.log(uploadedImageURL.url);
@@ -151,21 +167,34 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
     navigate(url);
   };
 
+  const eventType = onEditPage
+    ? events[id - 1].eventtype
+    : eventDetails.eventtype;
+
+  const getIcon = (type) => {
+    switch (type) {
+      case "Sports":
+        return <SportsBasketballIcon />;
+      case "Music":
+        return <MusicNoteIcon />;
+      case "Food":
+        return <LocalDiningIcon />;
+      case "Art":
+        return <ColorLensIcon />;
+      case "Hangout":
+        return <GroupsIcon />;
+      case "Gaming":
+        return <SportsEsportsIcon />;
+      default:
+        return null;
+    }
+  };
+
+  const eventIcon = getIcon(eventType);
+
   if (isMobile) {
     return (
-      <Box
-        component="section"
-        sx={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-          textAlign: "center",
-          overflowY: "auto",
-        }}
-      >
+      <Box>
         {/* Top App Bar */}
         <Box
           component="section"
@@ -210,155 +239,198 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
                 </IconButton>
               </Box>
 
-              <Box sx={{ flexGrow: 1 }} />
+              <Box sx={{ flexGrow: 2 }} />
             </Toolbar>
           </AppBar>
         </Box>
 
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            pt: "4.5rem",
+          }}
+        >
+          {onEditPage ? (
+            <Box
+              component="img"
+              id="EventReviewPhoto"
+              src={eventDetails.eventimage}
+            ></Box>
+          ) : (
+            <Box
+              component="img"
+              id="EventReviewPhoto"
+              src={
+                eventDetails.imageform === null ? logo : eventDetails.eventimage
+              }
+            ></Box>
+          )}
+        </Box>
+
         {/* Event Details */}
         <Box
-          component="form"
-          sx={{ width: "85%", height: "100%", pt: 8, textAlign: "left" }}
+          sx={{
+            px: 3, // Padding on left and right for the outer container
+          }}
         >
           {/* Event Title */}
-          <h1 id="EventReviewTitle">
-            {onEditPage ? events[id - 1].title : eventDetails.title}
-          </h1>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between", // Space between event detail and event picture
-              alignItems: "flex-start", // Align items to the top
-              margin: "0 auto",
-              "& p": {
-                // Apply styles to <p> tags inside this Box
-                margin: "4px 0", // Vertical margin for each <p> tag
-                padding: 0, // Remove any padding inside <p> tags
-              },
+          <Box>
+            <h1 id="EventReviewTitle">
+              {onEditPage ? events[id - 1].title : eventDetails.title}
+            </h1>
+          </Box>
+          {/* Date, time, and address */}
+          <Stack spacing={1}>
+            {/* Date */}
+            <Stack
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              color="text.secondary"
+            >
+              <CalendarMonthIcon />
+              <p id="EventReviewP">
+                {onEditPage ? events[id - 1].startdate : reviewDate}
+              </p>
+            </Stack>
+            {/* Time */}
+            <Stack
+              direction="row"
+              alignItems="center"
+              spacing={1}
+              color="text.secondary"
+            >
+              <AccessTimeIcon />
+              <p id="EventReviewP">{onEditPage ? reviewTime : reviewTime}</p>
+            </Stack>
+            {/* Address */}
+            <Stack
+              direction="row"
+              alignItems="flex-start"
+              spacing={1}
+              color="text.secondary"
+            >
+              <LocationOnIcon />
+              <p id="EventReviewP">
+                {onEditPage ? events[id - 1].address : eventDetails.address}
+              </p>
+            </Stack>
+          </Stack>
+
+          {/* Event type */}
+          <h1
+            id="EventReviewHeader"
+            style={{
+              marginBottom: 0,
+              paddingBottom: 0,
             }}
           >
-            {/* Left Group: Date, Time, Location */}
-            <Box
-              sx={{
-                flex: 2.5,
-              }}
-            >
-              <Box
-                color="text.secondary"
-                sx={{ display: "flex", alignItems: "center" }}
-              >
-                <CalendarMonthIcon />
-                {/* Adjust marginLeft later */}
-                <p id="EventReviewP" style={{ marginLeft: "8px" }}>
-                  {onEditPage ? events[id - 1].startdate : reviewDate}
-                </p>
-              </Box>
-              <Box
-                color="text.secondary"
-                sx={{ display: "flex", alignItems: "center" }}
-              >
-                <AccessTimeIcon />
-                {/* Adjust marginLeft later */}
-                <p id="EventReviewP" style={{ marginLeft: "8px" }}>
-                  {onEditPage ? reviewTime : reviewTime}
-                </p>
-              </Box>
-              <Box
-                color="text.secondary"
-                sx={{ display: "flex", alignItems: "center" }}
-              >
-                <LocationOnIcon />
-                {/* Adjust marginLeft later */}
-                <p id="EventReviewP" style={{ marginLeft: "8px" }}>
-                  {onEditPage ? events[id - 1].address : eventDetails.address}
-                </p>
-              </Box>
-            </Box>
-            {/* Right Group: Picture */}
-            <Box
-              sx={{
-                flex: 1,
-                display: "flex",
-                justifyContent: "flex-end", // Align picture to the right
-              }}
-            >
-              {/* Picture goes here */}
-              {onEditPage ? (
-                <Box
-                  component="img"
-                  id="EventReviewPhotoDesktop"
-                  src={eventDetails.eventimage}
-                ></Box>
-              ) : (
-                <Box
-                  component="img"
-                  id="EventReviewPhotoDesktop"
-                  src={eventDetails.imageform === null ? logo : eventDetails.eventimage}
-                ></Box>
-              )}
-            </Box>
-          </Box>
-          {/* Details */}
+            Event Type
+          </h1>
+          {/* Event Type Tag */}
           <Box
             sx={{
-              "& h1": { mb: 1 }, // margin bottom of 8px
-              "& p": { mt: 0 }, // margin top of 0px
+              display: "inline-flex", // Tag doesn't take up full container width, only its content
+              alignItems: "center",
+              gap: "0.5rem", // Space between mui icon / tag
+              marginTop: "0.5rem",
+              marginBottom: "0.5rem",
+              backgroundColor: "#ff7474", // color
+              borderRadius: "20px",
+              padding: "0.5rem 1rem", // make button bigger
+              color: "white", // font color
+              fontWeight: "bold",
+              textTransform: "uppercase", // Make text all caps (to match the filters box on homepage)
+            }}
+          >
+            {eventIcon}
+            {eventType}
+          </Box>
+
+          {/* Event Description */}
+          <Box
+            sx={{
+              "& h1": { mb: 0 }, //
+              "& p": { mt: 0 },
             }}
           >
             <h1 id="EventReviewHeader">Event Description</h1>
-            {/* Change the line below eventually */}
             <p id="EventReviewP">
               {onEditPage
                 ? events[id - 1].description
                 : eventDetails.description}
             </p>
           </Box>
+        </Box>
 
+        <Box
+          sx={{
+            px: 3, // Padding on left and right for the outer container
+          }}
+        >
           {/* Guest List */}
           <h1 id="EventReviewHeader">Guest List</h1>
 
           {/* Map Contacts */}
-          {invitedGuests.length === 0 ? "invite some friends loser" : invitedGuests.map((id, index) => {
-            const contact = contacts.find(contact => contact.id === id);
-            return (
-              <Box
-                sx={{ display: "flex", alignItems: "center", marginBottom: "16px" }}
-                key={id}
-              >
-                <div className="ReviewEventPage-desktop-avatar">
-                  {contact.name[0].toUpperCase()}
-                  {contact.name.split(" ")[1][0].toUpperCase()}
-                </div>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <h1 id="EventReviewNames">{contact.name}</h1>
+          {invitedGuests.length === 0 ? (
+            <p id="EventReviewP" style={{ marginTop: 0 }}>
+              Consider inviting some guests!
+            </p>
+          ) : (
+            invitedGuests.map((id, index) => {
+              const contact = contacts.find((contact) => contact.id === id);
+              return (
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginBottom: "16px",
+                  }}
+                  key={id}
+                >
+                  <div className="ReviewEventPage-desktop-avatar">
+                    {contact.name[0].toUpperCase()}
+                    {contact.name.split(" ")[1][0].toUpperCase()}
+                  </div>
+                  <Box sx={{ display: "flex", flexDirection: "column" }}>
+                    <h1 id="EventReviewNames">{contact.name}</h1>
+                  </Box>
                 </Box>
-              </Box>
-            );
-          })
-          }
-
+              );
+            })
+          )}
         </Box>
-        <Button
-          variant="contained"
-          onClick={() => navigate("/user/1")}
+        <Box
           sx={{
-            borderRadius: "10px",
-            // width: "25%", // button width
-            mb: 10,
-            mt: 4,
-            alignSelf: "center", // centers button
-            padding: "1rem", // button height
-            backgroundColor: "#F68F8D",
-            "&:hover": {
-              backgroundColor: "#A50B07",
-            },
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          Post Event and Send Invites
-        </Button>
+          <Button
+            variant="contained"
+            onClick={() => navigate("/user/1")}
+            sx={{
+              borderRadius: "10px",
+              mx: 3, // margin on left/right
+              width: "100%", // button width
+              mb: 10,
+              mt: 2,
+              alignSelf: "center", // centers button
+              padding: "1rem", // button height
+              backgroundColor: "#F68F8D",
+              "&:hover": {
+                backgroundColor: "#A50B07",
+              },
+            }}
+          >
+            Post Event and Send Invites
+          </Button>
+        </Box>
       </Box>
     );
   }
+
   // ---------DESKTOP VERSION---------------
   else {
     return (
@@ -465,7 +537,9 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
                         id="EventReviewDateTimeLocationDesktop"
                         style={{ marginLeft: "8px" }}
                       >
-                        {onEditPage ? events[id - 1].address : eventDetails.address}
+                        {onEditPage
+                          ? events[id - 1].address
+                          : eventDetails.address}
                       </p>
                     </Box>
                   </Box>
@@ -484,7 +558,11 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
                     <Box
                       component="img"
                       id="EventReviewPhotoDesktop"
-                      src={eventDetails.imageform === null ? logo : eventDetails.eventimage}
+                      src={
+                        eventDetails.imageform === null
+                          ? logo
+                          : eventDetails.eventimage
+                      }
                     ></Box>
                   )}
                 </Box>
@@ -520,27 +598,34 @@ const ReviewEventPage = ({ eventDetails, setEventDetails, detailsCompleted, invi
                   flexWrap: "wrap",
                 }}
               >
-
                 {/* Map Guests */}
-                {invitedGuests.length === 0 ? "invite some friends loser" : invitedGuests.map((id, index) => {
-                  const contact = contacts.find(contact => contact.id === id);
-                  return (
-                    <Box
-                      sx={{ display: "flex", alignItems: "center", marginBottom: 2 }}
-                      key={id}
-                    >
-                      <div className="ReviewEventPage-desktop-avatar">
-                        {contact.name[0].toUpperCase()}
-                        {contact.name.split(" ")[1][0].toUpperCase()}
-                      </div>
-                      <Box sx={{ display: "flex", flexDirection: "column" }}>
-                        <h1 id="EventReviewNamesDesktop">{contact.name}</h1>
-                      </Box>
-                    </Box>
-                  );
-                })
-                }
-
+                {invitedGuests.length === 0
+                  ? "invite some friends loser"
+                  : invitedGuests.map((id, index) => {
+                      const contact = contacts.find(
+                        (contact) => contact.id === id
+                      );
+                      return (
+                        <Box
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            marginBottom: 2,
+                          }}
+                          key={id}
+                        >
+                          <div className="ReviewEventPage-desktop-avatar">
+                            {contact.name[0].toUpperCase()}
+                            {contact.name.split(" ")[1][0].toUpperCase()}
+                          </div>
+                          <Box
+                            sx={{ display: "flex", flexDirection: "column" }}
+                          >
+                            <h1 id="EventReviewNamesDesktop">{contact.name}</h1>
+                          </Box>
+                        </Box>
+                      );
+                    })}
               </Box>
             </Stack>
           </Box>
